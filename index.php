bdc04ad81550981d860dbc5b8a02140ce46177c9
Alexander Galkin (BING USA) <algalkin@microsoft.com>
2017-01-03 01:00:32 -0800

    Split the original file into different includes

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Laurel School STEAM Fair</title>
+    <?php
+        include 'shared\head.php';
+    ?>
+
+</head>
+
+<body id="page-top" class="index">
+
+    <?php
+        include 'shared\nav.php';
+        include 'shared\top.php';
+        include 'shared\footer.php';
+        include 'shared\scripts.php';
+    ?>
+
+
+</body>
+
+</html>
---
+++ shared/footer.php
@@ -0,0 +1,51 @@
+    <!-- Footer -->
+    <footer class="text-center">
+        <div class="footer-above">
+            <div class="container">
+                <div class="row">
+                    <div class="footer-col col-md-4">
+                        <h3>Time and Location</h3>
+                        <p> <b>Friday, April 28, 2017</b>
+                        <p>Laurel Main Campus (K-3)
+                            <br> 95 Edge Road Atherton, CA 94027 </p>
+                        <p>Laurel Upper Campus (4-5)
+                            <br> 275 Elliott Dr, Menlo Park, CA 94025</p>
+                            
+                    </div>
+                    <div class="footer-col col-md-4">
+                        <h3>Around the Web</h3>
+                        <ul class="list-inline">
+                            <li>
+                                <a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-facebook"></i></a>
+                            </li>
+                            <li>
+                                <a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-google-plus"></i></a>
+                            </li>
+                            <li>
+                                <a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-twitter"></i></a>
+                            </li>
+                            <li>
+                                <a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-linkedin"></i></a>
+                            </li>
+                            <li>
+                                <a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-dribbble"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="footer-col col-md-4">
+                        <h3>About STEAM Fair</h3>
+                        <p>STEAM Fair is an annual event that is organized by parent volonteers of <a href="http://district.mpcsd.org/Domain/9">Laurel School</a> where students can learn some aspects of STEAM through hands-on workshops.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer-below">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        Copyright &copy; Laurel School STEAM parent commette, 2017
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
