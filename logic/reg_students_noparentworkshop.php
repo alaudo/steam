1b6f8639a75ae86b0866b3674baa990241c992e7
Alexander Galkin (BING USA) <algalkin@microsoft.com>
2017-03-09 08:56:57 -0800

    Changed wording

--- logic/reg_students_noparentworkshop.php
+++ logic/reg_students_noparentworkshop.php
@@ -3,7 +3,7 @@
                 <div class="alert alert-warning" role="alert">
                     <span class="glyphicon glyphicon-warning-sign" aria-hidden="true"></span>
                     <span class="sr-only">Warning:</span>
-                    You didn't volunteer to help with any workshops
+                    You have not signed up to volunteer for this grade.
                 </div>
             </div>
         </div>
